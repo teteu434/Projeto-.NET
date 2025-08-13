c95454444e16667f4eb33ab6403da54ccaa5e416
Matheus Henrique <matheushcosta434@gmail.com>
2025-08-13 00:45:14 -0300

    mudanças conexão db

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
+using DotNetEnv;
 using SistemaMatheus.Data;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
-var connectionString = builder.Configuration.GetConnectionString("MeuBanco");
-builder.Services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(connectionString));
+var connectionString =
+    $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+    $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+    $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+    $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+    $"Password={Environment.GetEnvironmentVariable("DB_PASS")}";
+
+// Sobrescreve no Configuration
+builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
+
+// Configura o EF Core
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Swagger config
 builder.Services.AddEndpointsApiExplorer();
